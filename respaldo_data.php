f8b2341c8fad1998d7459c95a33dfcd23d7e14e1
Andrés Padrón <andrespadron1398@gmail.com>
2023-02-03 12:33:07 -0400

    update

--- respaldo_data.php
+++ respaldo_data.php
@@ -12,24 +12,24 @@ if( $conn ) {
 
 $sql_exportar = "";
 
-//Empaque_Pedidos
-$sql = "SELECT * FROM Empaques_Pedidos ORDER BY Id_Pedido ASC";
-$stmt = sqlsrv_query( $conn, $sql );
-
-while ($row = sqlsrv_fetch_object( $stmt)) {
-    $sql_exportar .= "INSERT INTO `Empaques_Pedidos`(`Id_Pedido`, `Producto`, `Unidades_Empaque`, `Empaque_Pedido`) VALUES ('$row->Id_Pedido', '$row->Producto', '$row->Unidades_Empaque', '$row->Empaque_Pedido');\n";
-}
-//Fin de Empaque_Pedidos
-
-// //Pedido_Detalle
-// $sql = "SELECT * FROM Pedido_Detalle ORDER BY Id_Pedido ASC";
+// //Empaque_Pedidos
+// $sql = "SELECT * FROM Empaques_Pedidos ORDER BY Id_Pedido ASC";
 // $stmt = sqlsrv_query( $conn, $sql );
 
 // while ($row = sqlsrv_fetch_object( $stmt)) {
-//     $row->UltimaVenta = $row->UltimaVenta->format('Y-m-d H:i:s');
-//     $sql_exportar .= "INSERT INTO `Pedido_Detalle`(`Id_Pedido`, `Id_Producto`, `UltimaVenta`, `Sugerencia`, `Pedido`, `ExistenciaTeorica`, `CostoUnitario`) VALUES ('$row->Id_Pedido', '$row->Id_Producto', '$row->UltimaVenta', '$row->Sugerencia', '$row->Pedido', '$row->ExistenciaTeorica', '$row->CostoUnitario');\n";
+//     $sql_exportar .= "INSERT INTO `Empaques_Pedidos`(`Id_Pedido`, `Producto`, `Unidades_Empaque`, `Empaque_Pedido`) VALUES ('$row->Id_Pedido', '$row->Producto', '$row->Unidades_Empaque', '$row->Empaque_Pedido');\n";
 // }
-// //Pedido_Detalle
+// //Fin de Empaque_Pedidos
+
+//Pedido_Detalle
+$sql = "SELECT * FROM Pedido_Detalle ORDER BY Id_Pedido ASC";
+$stmt = sqlsrv_query( $conn, $sql );
+
+while ($row = sqlsrv_fetch_object( $stmt)) {
+    $row->UltimaVenta = $row->UltimaVenta->format('Y-m-d H:i:s');
+    $sql_exportar .= "INSERT INTO `Pedido_Detalle`(`Id_Pedido`, `Id_Producto`, `UltimaVenta`, `Sugerencia`, `Pedido`, `ExistenciaTeorica`, `CostoUnitario`) VALUES ('$row->Id_Pedido', '$row->Id_Producto', '$row->UltimaVenta', '$row->Sugerencia', '$row->Pedido', '$row->ExistenciaTeorica', '$row->CostoUnitario');\n";
+}
+//Pedido_Detalle
 
 
 $myfile = fopen("exportaciones/bbdd.sql", "w") or die("Unable to open file!");
