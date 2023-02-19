ade311bb5e1dfe9f5c32b78d0a38123ca4649f5c
Andres Padron <andrespadron1398@gmail.com>
2023-02-19 09:48:23 -0300

    update

--- respaldo_be.php
+++ respaldo_be.php
@@ -2,16 +2,34 @@
 //INICIO DEL SCRIPT
 include("alt_autoload.php-dist");
 $config = include('config/config.php');
+$ruta = "C:\AceptaService\simi_prod\pdf";
 
-if ($handle = opendir('C:\AceptaService\simi_prod\pdf')) {
+$parser = new \Smalot\PdfParser\Parser();
+
+if ($handle = opendir($ruta)) {
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
+            $pdf = $parser->parseFile($ruta.'\\'.$entry);
+            $text = $pdf->getText();
 
-            echo "$entry\n";
+            $numero_remision = $this->buscar_texto_entre($text, "REMISION:", "Nro. Caja:");
+            $n_boleta = $this->buscar_texto_entre($text, "Nro. Boleta:", "Hora");
+            $n_caja = $this->buscar_texto_entre($text, "Nro. Caja:", "Fecha");
+
+            echo "$numero_remision - $n_boleta - $n_caja<br>";
         }
     }
 
     closedir($handle);
+}
+
+function buscar_texto_entre($texto, $inicio, $termino){
+    $texto = ' ' . $texto;
+    $ini = strpos($texto, $inicio);
+    if ($ini == 0) return '';
+    $ini += strlen($inicio);
+    $len = strpos($texto, $termino, $ini) - $ini;
+    return substr($texto, $ini, $len);
 }
 // mysqli_close($enlace);
 ?>
