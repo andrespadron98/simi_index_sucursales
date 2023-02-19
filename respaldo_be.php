e01411265d315bc384dd007f83a957eb6b012167
Andres Padron <andrespadron1398@gmail.com>
2023-02-19 09:55:04 -0300

    update

--- respaldo_be.php
+++ respaldo_be.php
@@ -9,14 +9,23 @@ $parser = new \Smalot\PdfParser\Parser();
 if ($handle = opendir($ruta)) {
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
-            $pdf = $parser->parseFile($ruta.'\\'.$entry);
-            $text = $pdf->getText();
-
-            $numero_remision = buscar_texto_entre($text, "REMISION:", "Nro. Caja:");
-            $n_boleta = buscar_texto_entre($text, "Nro. Boleta:", "Hora");
-            $n_caja = buscar_texto_entre($text, "Nro. Caja:", "Fecha");
-
-            echo "$numero_remision - $n_boleta - $n_caja<br>";
+            //Verificar que los archivos sean pdf y no tengan mas de 5 días de antiguedad
+            $fecha_archivo = date("Y-m-d", filemtime($ruta.'\\'.$entry));
+            $fecha_actual = date("Y-m-d");
+            $fecha_archivo = strtotime($fecha_archivo);
+            $fecha_actual = strtotime($fecha_actual);
+            $diferencia = $fecha_actual - $fecha_archivo;
+            $dias = floor($diferencia / (60 * 60 * 24));
+            if($dias <= 5){
+                $pdf = $parser->parseFile($ruta.'\\'.$entry);
+                $text = $pdf->getText();
+    
+                $numero_remision = buscar_texto_entre($text, "REMISION:", "Nro. Caja:");
+                $n_boleta = buscar_texto_entre($text, "Nro. Boleta:", "Hora");
+                $n_caja = buscar_texto_entre($text, "Nro. Caja:", "Fecha");
+    
+                echo "$numero_remision - $n_boleta - $n_caja<br>";
+            }
         }
     }
 
